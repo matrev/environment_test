f35cd0e34a4a84d23a147cdd5c3d59bdeac51f51
matrev <markanthonytrevinojr@gmail.com>
2021-09-30 21:30:21 +0000

    added unit and integration test for author and price attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,8 +5,37 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'Title', with: 'harry potter'
+    fill_in 'Author', with: 'J.K Rowling'
+    fill_in 'Price', with: '12.99'
     click_on 'Create Book'
     visit books_path
     expect(page).to have_content('harry potter')
+  end
+
+  scenario 'valid inputs except for title' do
+    visit new_book_path
+    fill_in 'Author', with: 'Seth Rogan'
+    fill_in 'Price', with: '14.99'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to_not have_content('Yearbook')
+  end
+
+  scenario 'valid inputs except for author' do
+    visit new_book_path
+    fill_in 'Title', with: 'Watchmen'
+    fill_in 'Price', with: '29.99'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to have_content('Alan Moore')
+  end
+
+  scenario 'valid inputs except for price' do
+    visit new_book_path
+    fill_in 'Title', with: 'Project Hail Mary'
+    fill_in 'Author', with: 'Andy Weir'
+    click_on 'Create Book'
+    visit books_path
+    expect(page).to_not have_content('19.99')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,15 +3,25 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'J.K. Rowling', price: 9.99)
   end
 
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a name' do
+  it 'is not valid without a title name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
     expect(subject).not_to be_valid
   end
 end
