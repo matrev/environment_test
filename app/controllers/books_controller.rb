9ab8ebacd70afce5a78b18592f98cc14a1ef120d
root <root@localhost>
2021-10-01 02:09:07 +0000

    added delete page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -47,8 +47,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def delete
+    set_book
+  end
+
   # DELETE /books/1 or /books/1.json
   def destroy
+    set_book
     @book.destroy
     respond_to do |format|
       format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
