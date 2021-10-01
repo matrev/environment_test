ec1f414a66c8a8b64a384d70d2992ec6adbd93e3
root <root@localhost>
2021-10-01 02:35:26 +0000

    adding author to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_214606) do
+ActiveRecord::Schema.define(version: 2021_10_01_023259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "price"
     t.date "published_date"
+    t.string "author"
   end
 
 end
